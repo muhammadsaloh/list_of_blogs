fe753bf5ca4d0984a5b2a75b88abca7482af9ad6
muhammadsaloh <salohiddindilmatov9@gmail.com>
2022-02-26 15:19:48 +0500

    Add routes for homepage

---
+++ src/routes.ts
@@ -0,0 +1,26 @@
+import React, { lazy } from "react";
+
+// App Pages
+const HomePage = lazy(() => import("./Pages/HomePage"));
+
+export enum Routes {
+  HOME = "/",
+}
+
+interface Route {
+  title: string;
+  path: string;
+  component: React.FC;
+}
+
+export const routes: {
+  app: Route[];
+} = {
+  app: [
+    {
+      path: Routes.HOME,
+      title: "Home",
+      component: HomePage,
+    },
+  ],
+};
