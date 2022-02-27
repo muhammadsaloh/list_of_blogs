ff314cf8f971576542bf5951f7b753bd04d87cb0
muhammadsaloh <salohiddindilmatov9@gmail.com>
2022-02-27 16:18:01 +0500

    Add SinglePost when onclick

--- src/views/home/home-page.tsx
+++ src/views/home/home-page.tsx
@@ -1,11 +1,14 @@
 import React from "react";
-import Posts from "../../services/requests/home-page";
+import { Posts, SinglePost } from "../../services/requests/home-page";
 import { Table, Tag, Button, Tooltip, Modal } from "antd";
 import { FileTextOutlined } from "@ant-design/icons";
+import "./home-page.css";
 
 const HomePage: React.FC = () => {
-  const [modal, setModal] = React.useState(false);
-  const {data, loading} = Posts();
+  const [modal, setModal] = React.useState<boolean>(false);
+  const [postId, setPostId] = React.useState<number>();
+  const PostsData = Posts();
+  const SinglePostData = SinglePost(postId);
 
   const columns = [
     {
@@ -26,7 +29,14 @@ const HomePage: React.FC = () => {
       title: "Action",
       render: (_: object, post: any) => (
         <Tooltip placement="bottom" title="Read">
-          <Button onClick={() => setModal(true)} size="small" type="primary">
+          <Button
+            onClick={() => {
+              setModal(true);
+              setPostId(post.userId);
+            }}
+            size="small"
+            type="primary"
+          >
             <FileTextOutlined />
           </Button>
         </Tooltip>
@@ -37,13 +47,35 @@ const HomePage: React.FC = () => {
   return (
     <div className="home_page">
       <Table
-        loading={loading}
-        dataSource={data}
+        loading={PostsData.loading}
+        dataSource={PostsData.data}
         columns={columns}
         rowKey="id"
       />
-      <Modal title="Post info" onCancel={() => setModal(false)} visible={modal} onOk={() => setModal(false)}>
-        Hello
+      <Modal
+        title="Post info"
+        onCancel={() => setModal(false)}
+        visible={modal}
+        onOk={() => setModal(false)}
+      >
+        {SinglePostData.loading ? (
+          <div>Loading...</div>
+        ) : (
+          <ul className="home_page-list">
+            <li className="home_page-list-item">
+              <b>Card Id:</b>
+              <p>{SinglePostData.data?.id}</p>
+            </li>
+            <li className="home_page-list-item">
+              <b>User Id:</b>
+              <p>{SinglePostData.data?.userId}</p>
+            </li>
+            <li className="home_page-list-item">
+              <b>Title:</b>
+              <p>{SinglePostData.data?.title}</p>
+            </li>
+          </ul>
+        )}
       </Modal>
     </div>
   );
